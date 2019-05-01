327d5ebacbddcf55064b3488c5f5aa1da27fce66
Travis Exline <t.exline22@gmail.com>
2019-05-01 17:02:10 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -8,7 +8,7 @@ def oxford_comma(array)
     array.insert(3, ", and ")
     array.join
   else array.length > 3
-    array.insert(5, " and ")
+    array.insert(4, " and ")
     array.join(", ")
   end
 end
